d08e47dbdbde458554dd8555c81fe52fa33062da
biiCode37 <biicode37@gmail.com>
2026-07-14 00:08:55 +0700

    chore: add COOP headers to fix google auth popup warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+    }
+  },
+  preview: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+    }
+  },
   plugins: [
     react(),
     VitePWA({
